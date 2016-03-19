e65fb197322dd81472137dc20cb4109df13c79fb
wangxuefei <wangxuefii@163.com>
2016-03-19 15:56:09 +0800

    test commit

--- src/lintcode/linkedList/ListNode.java
+++ src/lintcode/linkedList/ListNode.java
@@ -9,4 +9,11 @@ public class ListNode {
         this.val = val;
         this.next = null;
     }
+    
+    /**
+     * test
+     */
+    public static void main(String[] args) {
+        System.out.println(0);
+    }
 }
